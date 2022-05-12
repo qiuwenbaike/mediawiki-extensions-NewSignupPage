13a95c9e9a5390711c7a711fbdd0ec9668a7f28a
WaitSpring <waitspring@git.qiuwen.wiki>
2022-05-12 11:56:18 +0800

    更新 'resources/js/NewSignupPage.js'

--- resources/js/NewSignupPage.js
+++ resources/js/NewSignupPage.js
@@ -6,7 +6,7 @@
 
 // Mandatory check
 (function () {
-	var a = document.getElementById("wpName1") || document.getElementById("wpName2") ? document.getElementById("wpLoginAttempt") || document.getElementById("wpCreateaccount") : false;
+	var a = document.getElementById("mw-input-wpTermsOfService") ? document.getElementById("wpLoginAttempt") || document.getElementById("wpCreateaccount") : false;
 		a && (a.disabled = !0, document.getElementById("mw-input-wpTermsOfService").checked = !1, document.getElementById("mw-input-wpTermsOfService").addEventListener("change", function () {
 		a.disabled = !0 === document.getElementById("mw-input-wpTermsOfService").checked ? !1 : !0;
 	}));
