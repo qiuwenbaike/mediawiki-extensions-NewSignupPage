acf07aabd3bc7e9bc6ae7baceeff7c00dc6257b8
WaitSpring <me@waitspring.com>
2022-11-09 15:00:49 +0800

    lint the code
    Signed-off-by: WaitSpring <me@waitspring.com>

--- resources/js/NewSignupPage.js
+++ resources/js/NewSignupPage.js
@@ -1,13 +1,19 @@
 /**
+ *
  * JavaScript file for performing some interactive and client-side validation.
+ *
  * @file
  * @copyright Copyright © 2022- Qiuwen Baike Contributors
  */
 
 // Mandatory check
 (function () {
-	var a = document.getElementById("mw-input-wpTermsOfService") ? document.getElementById("wpLoginAttempt") || document.getElementById("wpCreateaccount") : false;
-		a && (a.disabled = !0, document.getElementById("mw-input-wpTermsOfService").checked = !1, document.getElementById("mw-input-wpTermsOfService").addEventListener("change", function () {
-		a.disabled = !0 === document.getElementById("mw-input-wpTermsOfService").checked ? !1 : !0;
-	}));
-})();
+var a = document.getElementById('mw-input-wpTermsOfService') ? document.getElementById('wpLoginAttempt') || document.getElementById('wpCreateaccount') : false;
+if (a) {
+	a.disabled = !0;
+	document.getElementById('mw-input-wpTermsOfService').checked = !1;
+	document.getElementById('mw-input-wpTermsOfService').addEventListener('change', function () {
+		a.disabled = !0 === document.getElementById('mw-input-wpTermsOfService').checked ? !1 : !0;
+	});
+}
+}());
