714b446a56f4c59029302a56d333e263fd7a0858
WaitSpring <me@waitspring.com>
2023-03-04 11:50:59 +0800

    format
    Signed-off-by: WaitSpring <me@waitspring.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 /* eslint-env node, es6 */
-module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-eslint' );
-	grunt.loadNpmTasks( 'grunt-banana-checker' );
+module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-eslint');
+	grunt.loadNpmTasks('grunt-banana-checker');
 
-	grunt.initConfig( {
+	grunt.initConfig({
 		banana: {
 			all: 'i18n/'
 		},
@@ -11,14 +11,10 @@ module.exports = function ( grunt ) {
 			options: {
 				cache: true
 			},
-			all: [
-				'**/*.json',
-				'!node_modules/**',
-				'!vendor/**'
-			]
+			all: [ '**/*.json', '!node_modules/**', '!vendor/**' ]
 		}
-	} );
+	});
 
-	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
-	grunt.registerTask( 'default', 'test' );
+	grunt.registerTask('test', [ 'eslint', 'banana' ]);
+	grunt.registerTask('default', 'test');
 };
