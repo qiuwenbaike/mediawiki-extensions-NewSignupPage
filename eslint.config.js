b351c36d2d9a4d7408bb6b19c8bcdc578e8dcb2f
WaitSpring <me@waitspring.com>
2025-11-09 09:07:37 +0800

    add compat modules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
+import { fixupConfigRules } from "@eslint/compat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,10 +27,12 @@ export default [
 			'**/pnpm-lock.yaml'
 		]
 	},
-	...compat.extends(
-		'wikimedia/client',
-		'wikimedia/language/es2022',
-		'wikimedia/mediawiki'
+	...fixupConfigRules(
+		...compat.extends(
+			'wikimedia/client',
+			'wikimedia/language/es2022',
+			'wikimedia/mediawiki'
+		),
 	),
 	{
 		languageOptions: {
